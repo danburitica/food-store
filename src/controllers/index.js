e3c0798624f8786f2e26ef930c9d3213e03f3d92
Daniel Buriticá <daniel.buritica@ua.la>
2022-02-24 00:47:14 -0500

    Create controllers file and methods

---
+++ src/controllers/index.js
@@ -0,0 +1,62 @@
+const JSONdb = require("simple-json-db");
+const db = new JSONdb("./database.json");
+const axios = require("axios");
+require("dotenv").config();
+
+const { BUY_BASE_URL } = process.env;
+
+/**
+ * Es método es invocado por la cocina, al solicitar un nuevo plato con sus ingredientes.
+ * Verifica la base de datos local donde está el stock de alimentos, si tiene la cantidad suficiente, despacha.
+ * Si no tiene stock, compra en la plaza, hasta que tenga la cantidad mínima o más.
+ */
+
+const getIngredients = async (req, res) => {
+  const { ingredients } = req.body;
+  const database = db.JSON();
+
+  let ingredientsToKitchen = {};
+
+  for (const ingredient in ingredients) {
+    let reqQuantity = ingredients[ingredient];
+    let storeQuantity = database[ingredient];
+    if (ingredient in database) {
+      if (storeQuantity - reqQuantity >= 0) {
+        ingredientsToKitchen[ingredient] = reqQuantity;
+        database[ingredient] = storeQuantity - reqQuantity;
+      } else {
+        const newQuantity = await buyIngredients(
+          ingredient,
+          reqQuantity - storeQuantity
+        );
+        ingredientsToKitchen[ingredient] = reqQuantity;
+        database[ingredient] = newQuantity + storeQuantity - reqQuantity;
+      }
+    }
+  }
+  db.JSON(database);
+  db.sync();
+  res.json(ingredientsToKitchen);
+};
+
+/**
+ * Método auxiliar que es invocado desde getIngredients.
+ * Se encarga de pedir datos al endpoint donde se encuentra la plaza de mercado.
+ * Se realiza la petición, mientras la compra no sea exitosa (quantitySold = 0) o la compra sea menor a la cantidad mínima requerida.
+ */
+
+const buyIngredients = async (ingredient, minQuantity) => {
+  let quantityBuy = 0;
+  while (quantityBuy < minQuantity) {
+    const {
+      data: { quantitySold },
+    } = await axios.get(BUY_BASE_URL + ingredient);
+    quantityBuy += quantitySold;
+  }
+
+  return quantityBuy;
+};
+
+module.exports = {
+  getIngredients,
+};
